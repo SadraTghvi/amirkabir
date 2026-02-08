77658bc9616dbcf6ff3eeb5b2a9271b6bf76da08
sadra <sadrataghavi1383@gmail.com>
2026-02-08 22:43:35 +0330

    'dist'

--- src/utils/tools.ts
+++
@@ -1,6 +0,0 @@
-export function deepcopy<T>(value: T): T {
-    return JSON.parse(JSON.stringify(value))
-}
-
-export const sleep = (ms: number) =>
-    new Promise(resolve => setTimeout(resolve, ms))
